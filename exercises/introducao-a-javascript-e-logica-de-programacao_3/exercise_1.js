63bae3c7859db73e0c14f9d454293e171c5e8412
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-02-26 19:46:17 -0300

    inicio da atividade 1

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/exercise_1.js
@@ -0,0 +1,12 @@
+// criar quadrado usado n>1, sendo quadro a quantidade de n
+// n>1
+// para cada repetição incluir a quantidade de * na linha e repetir a mesma quantidade de vezes
+
+let tamanhoQuadrado = 2
+//se n>1 apresentar erro
+if (tamanhoQuadrado<1){
+    console.log("Erro: Insira um n>1")
+    
+} else(){
+    for (1;2;3)
+}
