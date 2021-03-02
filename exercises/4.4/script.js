153fd39fe044ea1fdc723f026b4e74f75b31f08b
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-03-02 11:25:58 -0300

    inclusao do exercicio 4.4

---
+++ exercises/4.4/script.js
@@ -0,0 +1,17 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+  info.recorrente = "Sim";
+
+  let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Chistmas on Bear Mountain, Dell1s Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente= "Sim",
+  };
+
+  for (let key in info) {
+    
+}
